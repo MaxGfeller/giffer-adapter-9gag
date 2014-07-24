0402f6078ae839fdf5e11b4c85e62c1f5e893fd7
MaxGfeller <max.gfeller@gmail.com>
2014-07-24 16:31:20 +0200

    Pass a parameter to specify number of pages to grab

--- index.js
+++ index.js
@@ -6,41 +6,50 @@ var request = require('request')
 
 inherits(Adapter, EventEmitter)
 
-function Adapter(config) {
-  this.page = config.page || 'hot'
-  this.apiEndpoint = config.apiEndpoint || 'http://infinigag.eu01.aws.af.cm'
-  EventEmitter.call(this)
-}
+    function Adapter(config) {
+        this.page = config.page || 'hot'
+        this.apiEndpoint = config.apiEndpoint || 'http://infinigag.eu01.aws.af.cm'
+        this.maxPages = config.maxPages || 1
+        EventEmitter.call(this)
+    }
 
 Adapter.prototype.start = function() {
-  this.emit('start')
-  this.currentPage = 0
+    this.emit('start')
+    this._numOfPages = 0
 
-  var self = this
-  request(this.apiEndpoint + '/' + this.page + '/0', function(err, res, body) {
-    if(err) throw err
-
-    var obj = JSON.parse(body)
-    obj.data.forEach(function(gag) {
-      request(gag.link, function (err, res, body) {
-        if(err) throw err
-
-        // console.log(body.match(/|http:\/\/d24w6bsrhbeh9d\.cloudfront\.net\/photo\/.+?\.gif/g))
-        body.split(' ').forEach(function(str) {
-            if(str.indexOf('cloudfront.net/photo') > -1
-                && str.indexOf('src') > -1
-                && str.indexOf('.gif') > -1) {
-                    self.emit('gif', str.substring(str.indexOf('"')).replace(/"/g, ''))
-                }
-        })
-      })
-    })
-  })
+    this._getPage('0')
 }
 
 Adapter.prototype.stop = function() {
-  this.emit('stop')
-  // stop grabbing gifs
+    this.emit('stop')
+}
+
+Adapter.prototype._getPage = function(id) {
+    var self = this
+    request(this.apiEndpoint + '/' + this.page + '/' + id, function(err, res, body) {
+        if (err) throw err
+
+        var obj = JSON.parse(body)
+
+        obj.data.map(function(gag) {
+            request(gag.link, function(err, res, body) {
+                if (err) throw err
+
+                body.split(' ').forEach(function(str) {
+                    if (str.indexOf('cloudfront.net/photo') > -1 && str.indexOf('src') > -1 && str.indexOf('.gif') > -1) {
+                        self.emit('gif', str.substring(str.indexOf('"')).replace(/"/g, ''))
+                    }
+                })
+            })
+        })
+
+        this._numOfPages++
+
+        if (this._numOfPages < this.maxPages)
+            return this._getPage(obj.paging.next)
+
+        this.stop()
+    }.bind(this))
 }
 
 module.exports = Adapter
